026f93acf7d2660e66ba5ce0df9a0c379f1d9769
ajamaking <157380694+ajamaking@users.noreply.github.com>
2024-06-02 15:56:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from config import bot_token
 
 TOKEN = bot_token
 securitylab_url = 'https://www.securitylab.ru/news/'
-habr_url = 'https://habr.com/ru/rss/hub/just_ai/'
+habr_url = 'https://habr.com/ru/news/'
 
 bot = telebot.TeleBot(TOKEN)
 
-def parse_securitylab_news(news_count=5):
+def parse_securitylab_news(news_count=None):
     response = requests.get(securitylab_url)
     soup = BeautifulSoup(response.content, 'html.parser')
     articles_cards = soup.find_all("a", class_="article-card")
@@ -24,11 +24,18 @@ def parse_securitylab_news(news_count=5):
 
     return news_links
 
-def parse_habr_news():
+def parse_habr_news(news_count=None):
     response = requests.get(habr_url)
-    soup = BeautifulSoup(response.content, 'xml')
-    items = soup.find_all("item")
-    news_links = [{'title': item.find("title").text, 'link': item.find("link").text} for item in items[:5]]
+    soup = BeautifulSoup(response.content, 'html.parser')
+    articles_list = soup.find("div", class_="tm-articles-list")
+    articles = articles_list.find_all("article")
+
+    news_links = []
+    for article in articles[:news_count]:
+        title = article.find("h2",).text.strip()
+        link = "https://habr.com" + article.find("a", class_="tm-title__link").get("href")
+        news_links.append({'title': title, 'link': link})
+
     return news_links
 
 def save_to_json(news_links, filename):
@@ -44,29 +51,36 @@ def start(message):
 
 @bot.message_handler(func=lambda message: message.text in ['SecurityLab', 'Habr'])
 def choose_option(message):
+    site = message.text
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(types.KeyboardButton('Последняя новость'))
     markup.add(types.KeyboardButton('5 последних новостей'))
     markup.add(types.KeyboardButton('Все новости'))
+    markup.add(types.KeyboardButton('Назад'))
     bot.send_message(message.chat.id, 'Выберите опцию:', reply_markup=markup)
-    bot.register_next_step_handler(message, get_news, message.text)
+    bot.register_next_step_handler(message, get_news, site)
 
 def get_news(message, site):
     if site == 'SecurityLab':
-        news_links = parse_securitylab_news()
+        total_articles = len(parse_securitylab_news(None))
     elif site == 'Habr':
-        news_links = parse_habr_news()
+        total_articles = len(parse_habr_news(None))
 
     if message.text == 'Последняя новость':
-        news = news_links[:1]
+        news_count = 1
     elif message.text == '5 последних новостей':
-        news = news_links[:5]
+        news_count = 5
     elif message.text == 'Все новости':
-        news = news_links
+        news_count = total_articles
 
-    for article in news:
+    if site == 'SecurityLab':
+        news_links = parse_securitylab_news(news_count)
+    elif site == 'Habr':
+        news_links = parse_habr_news(news_count)
+
+    for article in news_links:
         bot.send_message(message.chat.id, f'<a href="{article["link"]}">{article["title"]}</a>', parse_mode='HTML')
-    save_to_json(news, f'{site.lower()}_news.json')
+    save_to_json(news_links, f'{site.lower()}_news.json')
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(types.KeyboardButton('Назад'))
